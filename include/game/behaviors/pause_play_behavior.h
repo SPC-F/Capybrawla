cd0f73c9d6ab91d29eafde831e4bab3dd1265f43
LarsDevans <l.beijaard1@student.avans.nl>
2025-12-05 14:51:28 +0100

    feat: add pause and play menu

---
+++ include/game/behaviors/pause_play_behavior.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "engine/core/engine.h"
+#include "engine/input/input_manager.h"
+#include "engine/public/behavior.h"
+
+class PausePlayBehavior final : public Behavior
+{
+public:
+    explicit PausePlayBehavior(const std::reference_wrapper<PauseMenuUI> pause_menu)
+        : input_(Engine::instance().services->get_service<InputManager>().get().provider()),
+          scene_service_(Engine::instance().services->get_service<SceneService>().get()),
+          pause_menu_(pause_menu.get())
+    {}
+
+    void on_awake() override
+    {
+        pause_menu_.set_inactive();
+        pause_menu_.set_resume_press_callback([this]()
+        {
+            is_paused_ = false;
+            update_ui_state();
+        });
+        pause_menu_.set_quit_press_callback([this]()
+        {
+            is_paused_ = false;
+            update_ui_state();
+        });
+    }
+
+    void on_update(float dt) override
+    {
+         if (input_.is_key_pressed(KeyCode::escape))
+         {
+            if (!scene_service_.current_scene().value().get().name().starts_with("Level_")) return;
+
+             is_paused_ = !is_paused_;
+             update_ui_state();
+         }
+    }
+
+private:
+    const IInputProvider& input_;
+    const SceneService& scene_service_;
+    bool is_paused_ = false;
+    PauseMenuUI& pause_menu_;
+
+    void update_ui_state() const
+    {
+        if (is_paused_) pause_menu_.set_active();
+        else pause_menu_.set_inactive();
+    }
+};
---
+++ include/game/pause_menu_ui.h
@@ -0,0 +1,126 @@
+#pragma once
+
+#include "engine/public/components/ui/image.h"
+#include "engine/public/components/ui/text.h"
+#include "engine/public/ui/ui_object.h"
+#include "engine/public/ui/interactable/ui_button.h"
+#include "engine/public/util/layers.h"
+
+constexpr float BUTTON_WIDTH = 400.0f;
+constexpr float BUTTON_HEIGHT = 80.0f;
+constexpr float BUTTON_FONT_SIZE = 46.0f;
+
+constexpr int SCREEN_WIDTH = 1920.0f;
+constexpr int SCREEN_HEIGHT = 1080.0f;
+
+constexpr int CENTER_X = SCREEN_WIDTH / 2;
+constexpr int CENTER_Y = SCREEN_HEIGHT / 2;
+
+constexpr int BUTTON_SPACING = 20;
+constexpr float BUTTON_START_Y = CENTER_Y - ((BUTTON_HEIGHT + BUTTON_SPACING) * 2);
+constexpr float BUTTON_START_X = CENTER_X - (BUTTON_WIDTH / 2);
+
+constexpr float TITLE_FONT_SIZE = 180.0f;
+
+class PauseMenuUI final : public UIObject
+{
+public:
+    using resume_press_callback_t = std::function<void()>;
+    using quit_press_callback_t = std::function<void()>;
+
+    explicit PauseMenuUI(Scene& scene)
+        : UIObject(scene, 1920, 1080, {0.0f, 0.0f}, {0.0f, 0.0f})
+    {
+        add_component<Image>("", false, false, 1920, 1080, Color{0, 0, 0, 128});
+
+        add_title();
+        add_resume_button(scene);
+        add_quit_button(scene);
+    }
+
+    void set_resume_press_callback(const resume_press_callback_t& resume_press_callback)
+    {
+        resume_press_callback_ = resume_press_callback;
+    }
+
+    void set_quit_press_callback(const quit_press_callback_t& quit_press_callback)
+    {
+        quit_press_callback_ = quit_press_callback;
+    }
+
+private:
+    resume_press_callback_t resume_press_callback_;
+    quit_press_callback_t quit_press_callback_;
+
+    void add_title()
+    {
+        add_component<Text>(
+                "Maybe him skill issue?",
+                "ByteBounce",
+                "resources/fonts/bytebounce/ByteBounce.ttf",
+                TITLE_FONT_SIZE,
+                Color{255, 255, 255, 255}
+            )
+            .alignment(Center)
+            .offset(Point{0.0f, -BUTTON_HEIGHT * 3});
+    }
+
+    void add_resume_button(Scene& scene)
+    {
+        UIButton& btn = create_button(
+            scene,
+            "Back to game",
+            BUTTON_START_X,
+            BUTTON_START_Y + (BUTTON_HEIGHT + BUTTON_SPACING) * 1,
+            "button_large_green"
+        );
+        btn.parent(*this);
+        btn.add_on_press([this](UIButton&) { resume_press_callback_(); });
+    }
+
+    void add_quit_button(Scene& scene)
+    {
+        UIButton& btn = create_button(
+            scene,
+            "Give up",
+            BUTTON_START_X,
+            BUTTON_START_Y + (BUTTON_HEIGHT + BUTTON_SPACING) * 2,
+            "button_large_red"
+        );
+        btn.parent(*this);
+        btn.add_on_press([this](UIButton&)
+        {
+            quit_press_callback_();
+            auto& scene_service = Engine::instance().services->get_service<SceneService>().get();
+            scene_service.load_scene("MainMenuScene");
+        });
+    }
+
+    UIButton& create_button(
+        Scene& scene,
+        const std::string& label,
+        float position_x,
+        float position_y,
+        const std::string& texture_id
+    ) {
+        auto& button = scene.add_game_object<UIButton>(
+            scene,
+            BUTTON_WIDTH,
+            BUTTON_HEIGHT,
+            Point{0.5f, 0.5f},
+            Point{0.5f, 0.5f},
+            label,
+            "ByteBounce",
+            "resources/fonts/bytebounce/ByteBounce.ttf",
+            texture_id
+        );
+        button.transform().position({position_x, position_y, 0.0f});
+        button.label_color(Color{255, 255, 255, 255});
+        button.font_size(BUTTON_FONT_SIZE);
+        button.layer(Layers::UI);
+
+        return button;
+    }
+};
+
+
