4290ee520b05572ab2872a53a3c872e06b1b15a2
Luka Leppens <60887692+BotSquirrel@users.noreply.github.com>
2025-12-22 12:22:48 +0100

    feat (weapons): add gloves and split functions

--- src/scenes/swamp.cpp
+++ src/scenes/swamp.cpp
@@ -13,6 +13,7 @@
 #include <game/prefabs/ai_drone_agent_object.h>
 #include <game/prefabs/weapons/weapon_bat_player_object.h>
 #include <game/prefabs/weapons/weapon_axe_player_object.h>
+#include <game/prefabs/weapons/weapon_boxing_gloves_player_object.h>
 #include <game/prefabs/weapons/weapon_sword_player_object.h>
 #include <game/round/roundcontroller.h>
 #include <game/scenes/level_loader.h>
@@ -30,8 +31,7 @@ void load_players(Scene& scene, RoundController& controller, float start_x = 100
       -SwampScene::out_of_bounds_margin_y,
       SwampScene::map_height + SwampScene::out_of_bounds_margin_y));
 
-    auto& weapon_axe = scene.add_game_object<WeaponAxePlayerObject>(scene, player);
-
+    auto& weapon_boxing_gloves = scene.add_game_object<WeaponBoxingGlovesPlayerObject>(scene, player);
     player.layer(Layers::Foreground);
 
     auto& ai_player = scene.add_game_object<PlayerObject>(scene, Vector3{start_x + 40.0f, 100.0f, 0}, false);
