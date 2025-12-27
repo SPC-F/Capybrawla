95a6bbc9d37c2522eec66db114ff1078028549a8
Luka Leppens <60887692+BotSquirrel@users.noreply.github.com>
2025-12-27 10:01:37 +0100

    fix (movement): remove temporary scale update fix

--- src/character/player_movement_behavior.cpp
+++ src/character/player_movement_behavior.cpp
@@ -52,9 +52,6 @@ void PlayerMovementBehavior::on_start() {
   sprite_opt_       = get_component<Sprite>();
   box_collider_opt_ = get_component<BoxCollider2D>();
 
-  /// TO BE REMOVED AFTER ENGINE PATCH
-  rigidbody_opt_->get().gravity_scale(3.0f);
-
   if (!player_has_required_components()) {
     throw std::runtime_error("PlayerMovementBehavior missing components");
   }
