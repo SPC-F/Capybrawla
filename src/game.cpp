4a12d1b66371d10b277be059c4f173b5557d391d
LarsDevans <l.beijaard1@student.avans.nl>
2025-12-29 10:28:57 +0100

    feat: add btn hover sound

--- src/game.cpp
+++ src/game.cpp
@@ -256,6 +256,7 @@ void Game::initialize() {
         {"./resources/sounds/start_menu.wav", "start_menu", SoundType::SDL_MIXER},
         {"./resources/sounds/spear_of_justice.wav", "spear_of_justice", SoundType::SDL_MIXER},
         {"./resources/sounds/enemy_approaching.wav", "enemy_approaching", SoundType::SDL_MIXER},
+        {"./resources/sounds/btn_hover.wav", "btn_hover", SoundType::SDL_MIXER},
     };
     Assets::register_audio(audios);
 
