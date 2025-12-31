68b26a8a4a8d04a3ea5e23e3a7c8fd115cb4f708
SjulGr <sjulgrooten@live.nl>
2025-12-31 10:36:01 +0100

    feat (item dropper): start timer after previous object was taken

--- include/game/behaviors/interactable/ItemDropper.h
+++ include/game/behaviors/interactable/ItemDropper.h
@@ -1,14 +1,17 @@
 #pragma once
 #include <engine/public/behavior.h>
 
+#include <string>
+
 class ItemDropper : public Behavior {
 private:
     std::vector<std::string> drops_;
+    std::string latest_obj_;
     constexpr static double default_drop_duration_secs = 15.0;
     double remaining_time_before_drop = default_drop_duration_secs;
 
 public:
-    [[nodiscard]] GameObject& random_drop() const;
+    [[nodiscard]] GameObject& random_drop();
     void on_start() override;
     void on_update(float dt) override;
 };
--- src/behaviors/interactables/ItemDropper.cpp
+++ src/behaviors/interactables/ItemDropper.cpp
@@ -9,12 +9,14 @@
 #include <game/prefabs/interactables/health_pack.h>
 #include <game/prefabs/weapons/weapon_axe_player_object.h>
 
-GameObject& ItemDropper::random_drop() const {
+GameObject& ItemDropper::random_drop() {
     const int random_index = std::rand() % drops_.size();
     auto& prefab_service = Engine::instance().services->get_service<PrefabService>().get();
     Scene& scene = Engine::instance().services->get_service<SceneService>().get().current_scene().value();
 
-    return prefab_service.instantiate(drops_[random_index], scene, uuid::generate_uuid_v4());
+    latest_obj_ = uuid::generate_uuid_v4();
+
+    return prefab_service.instantiate(drops_[random_index], scene, latest_obj_);
 }
 
 void ItemDropper::on_start() {
@@ -33,13 +35,24 @@ void ItemDropper::on_start() {
 
 void ItemDropper::on_update(float dt)
 {
-    remaining_time_before_drop -= dt;
-    if(remaining_time_before_drop <= 0) {
-        auto& drop = random_drop();
-        const auto position = game_object().transform().position();
-        const auto half_position = position - Vector3{16, 16, 0};
-        drop.get_component<Rigidbody2D>()->get().teleport(half_position);
+    bool latest_spawn_used = true;
+    for (auto& obj : game_object().scene().game_objects()) {
+        auto comp = obj.get().get_component<NetworkIdentity>();
+        if (comp.has_value() && latest_obj_ == comp.value().get().uuid()) {
+            latest_spawn_used = false;
+            break;
+        }
+    }
 
-        remaining_time_before_drop = default_drop_duration_secs;
+    if (latest_spawn_used) {
+        remaining_time_before_drop -= dt;
+        if(remaining_time_before_drop <= 0) {
+            auto& drop = random_drop();
+            const auto position = game_object().transform().position();
+            const auto half_position = position - Vector3{16, 16, 0};
+            drop.get_component<Rigidbody2D>()->get().teleport(half_position);
+
+            remaining_time_before_drop = default_drop_duration_secs;
+        }
     }
 }
