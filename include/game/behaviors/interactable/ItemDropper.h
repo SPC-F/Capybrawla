39f0e66336457b3f7758a5ed8657ae53fd307794
SjulGr <sjulgrooten@live.nl>
2025-12-31 09:47:12 +0100

    refactor (item dropper): change clone to prefab service

--- include/game/behaviors/interactable/ItemDropper.h
+++ include/game/behaviors/interactable/ItemDropper.h
@@ -3,12 +3,12 @@
 
 class ItemDropper : public Behavior {
 private:
-    std::vector<std::unique_ptr<GameObject>> drops_;
+    std::vector<std::string> drops_;
     constexpr static double default_drop_duration_secs = 15.0;
     double remaining_time_before_drop = default_drop_duration_secs;
 
 public:
-    [[nodiscard]] std::unique_ptr<GameObject> random_drop() const;
+    [[nodiscard]] GameObject& random_drop() const;
     void on_start() override;
     void on_update(float dt) override;
 };
--- src/behaviors/interactables/ItemDropper.cpp
+++ src/behaviors/interactables/ItemDropper.cpp
@@ -1,37 +1,44 @@
+#include <engine/core/engine.h>
+#include <engine/public/scene_service.h>
+#include <engine/public/components/rigidbody_2d.h>
+#include <engine/public/prefab_service.h>
+#include <engine/public/components/network_identity.h>
+#include <engine/util/uuid.h>
+
 #include <game/behaviors/interactable/ItemDropper.h>
+#include <game/prefabs/interactables/health_pack.h>
+#include <game/prefabs/weapons/weapon_axe_player_object.h>
 
-#include <iostream>
-
-#include "engine/core/engine.h"
-#include "engine/public/scene_service.h"
-#include "engine/public/components/rigidbody_2d.h"
-#include "game/prefabs/interactables/health_pack.h"
-#include "game/prefabs/weapons/weapon_axe_player_object.h"
-
-// std::unique_ptr<GameObject> ItemDropper::random_drop() const
-// {
-//     const int random_index = std::rand() % drops_.size();
-//     return drops_[random_index]->clone();
-// }
-
-void ItemDropper::on_start()
-{
+GameObject& ItemDropper::random_drop() const {
+    const int random_index = std::rand() % drops_.size();
+    auto& prefab_service = Engine::instance().services->get_service<PrefabService>().get();
     Scene& scene = Engine::instance().services->get_service<SceneService>().get().current_scene().value();
-    drops_.emplace_back(std::make_unique<HealthPackPrefab>(scene));
+
+    return prefab_service.instantiate(uuid::generate_uuid_v4(), scene, drops_[random_index]);
+}
+
+void ItemDropper::on_start() {
+    auto& prefab_service = Engine::instance().services->get_service<PrefabService>().get();
+    if (!prefab_service.has_prefab("HealthPack")) {
+        prefab_service.register_prefab("HealthPack", [this](Scene& scene, const std::string& name) -> GameObject& {
+            auto& obj = scene.add_game_object<HealthPackPrefab>(scene);
+            obj.add_component<NetworkIdentity>(name.c_str());
+
+            return obj;
+        });
+    }
+
+    drops_.emplace_back("HealthPack");
 }
 
 void ItemDropper::on_update(float dt)
 {
     remaining_time_before_drop -= dt;
-    if(remaining_time_before_drop <= 0)
-    {
-        // auto drop = random_drop();
+    if(remaining_time_before_drop <= 0) {
+        auto& drop = random_drop();
         const auto position = game_object().transform().position();
         const auto half_position = position - Vector3{16, 16, 0};
-        // drop->get_component<Rigidbody2D>()->get().teleport(half_position);
-
-        Scene& scene = Engine::instance().services->get_service<SceneService>().get().current_scene().value();
-        // scene.add_game_object(std::move(drop));
+        drop.get_component<Rigidbody2D>()->get().teleport(half_position);
 
         remaining_time_before_drop = default_drop_duration_secs;
     }
